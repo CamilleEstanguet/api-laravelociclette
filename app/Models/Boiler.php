9a6ebf4e5ff2d03bebe7176947252435995778b2
Camille Estanguet <estanguetcamille@gmail.com>
2022-03-31 12:30:54 +0200

    CreateTables (to finish)

---
+++ app/Models/Boiler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Boiler extends Model
+{
+    use HasFactory;
+}
